c3dfb4eadee3b26918b159a8404c4525e90f133e
SarathS95 <sarath9504@gmail.com>
2025-04-29 19:00:10 +1200

    Post

---
+++ fitztagram/frontend/fitztagram-react-app/src/components/Post.jsx
@@ -0,0 +1,37 @@
+import {Avatar, Box, Button, Stack, TextField} from "@mui/material";
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+import EmojiEnoticonsIcon from '@mui/icons-material/EmojiEmotions';
+import {useState} from "react";
+
+function Post() {
+    const [postText, setPostText] = useState('');
+
+    const handlePost = () => {
+        if (postText.trim() === "")return;
+
+        console.log("Post created:", postText)
+        setPostText('');
+    };
+
+    return(
+        <Box sx={{backgroundColor: 'white', padding: 3, borderRadius: 3, boxShadow: 3, mb: 3}}>
+            <Stack direction= "row" spacing={2} alignItems="center" mb={2}>
+                <Avatar />
+                <TextField fullWidth placeholder="What is your post of the day?" variant="outlined" value={postText} onChange= {(e) => setPostText(e.target.value)} sx={{backgroundColor: '#f0f2f5', borderRadius: 5}} />
+            </Stack>
+
+            <Stack direction="row" justifyContent="space-around" spacing={3}>
+                <Button startIcon={<VideoCallIcon sx={{color: 'red'}} />}>Post Video</Button>
+                <Button startIcon={<PhotoLibraryIcon sx={{color: 'red'}} />}>Post Image</Button>
+                <Button startIcon={<EmojiEnoticonsIcon sx={{color: 'orange'}} />}>Feelings</Button>
+            </Stack>
+
+            <Box sx = {{display: 'flex', justifyContent: 'center', mt: 2}}>
+                <Button variant= "contained" color="primary" onclick={handlePost}>Post</Button>
+            </Box>
+        </Box>
+    )
+}
+
+export default Post;
